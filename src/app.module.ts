54e92799bd6963d61decdc95ded1857563f2b50d
heal <heal@hyperhire.in>
2023-01-31 19:01:50 +0900

    Init: config 초기 셋팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import appConfig from "./config/app.config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod', // store produnction env on cicd
+      load: [
+        appConfig
+      ]
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
