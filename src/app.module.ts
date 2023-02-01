9cbb33fa0c27f1be13ebd7ef69832ef130c4af0d
heal <heal@hyperhire.in>
2023-02-01 20:39:28 +0900

    Init: User CRUD generator 실행/ typeorm entity 작동하는지 확인

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import * as DailyRotateFile from 'winston-daily-rotate-file';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './database/typeorm-config.service';
 import { DataSource } from 'typeorm';
+import { UsersModule } from './users/users.module';
 
 const logDir = 'logs';
 
@@ -64,6 +65,7 @@ const TypeOrmSettingModule = TypeOrmModule.forRootAsync({
     HealthModule,
     WinstomSettingModule,
     TypeOrmSettingModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
