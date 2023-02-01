50552867b3dcdbcd5fd5ee8b3f3bbb56fa3abacd
heal <heal@hyperhire.in>
2023-02-01 17:26:31 +0900

    Init: Logger Winston으로 셋팅

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger as WinstonLogger } from 'winston';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject(WINSTON_MODULE_PROVIDER)
+    private readonly logger: WinstonLogger,
+  ) {}
 
   @Get()
   getHello(): string {
+    this.logger.info('1111');
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,42 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import appConfig from './config/app.config';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
+import * as DailyRotateFile from 'winston-daily-rotate-file';
+
+const logDir = 'logs';
+
+const dailyLoggerOptions = (level: string) => {
+  return {
+    level,
+    datePattern: 'YYYY-MM-DD',
+    dirname: logDir + `/${level}`,
+    filename: `${level}-%DATE%.log`,
+    maxSize: '20m',
+    maxFiles: '14d',
+    zippedArchive: true,
+  };
+};
+
+const WinstomSettingModule = WinstonModule.forRoot({
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        nestWinstonModuleUtilities.format.nestLike('projectName', {
+          prettyPrint: true,
+        }),
+      ),
+    }),
+    new DailyRotateFile(dailyLoggerOptions('error')),
+    new DailyRotateFile(dailyLoggerOptions('warn')),
+    new DailyRotateFile(dailyLoggerOptions('info')),
+  ],
+});
 
 @Module({
   imports: [
@@ -14,6 +50,7 @@ import appConfig from './config/app.config';
       load: [appConfig],
     }),
     HealthModule,
+    WinstomSettingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
