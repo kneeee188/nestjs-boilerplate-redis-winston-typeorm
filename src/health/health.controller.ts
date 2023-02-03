c222f926b409f1016eba91ff06f9b17d31d53d0d
heal <heal@hyperhire.in>
2023-02-03 16:43:43 +0900

    chores: GET /health 불필요한 부분 제거

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,13 +1,7 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
-// import {
-//   HealthCheck,
-//   HealthCheckService,
-//   HttpHealthIndicator,
-// } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
-  //   constructor() {} // private http: HttpHealthIndicator, // private health: HealthCheckService,
 
   @Get()
   @HttpCode(200)
