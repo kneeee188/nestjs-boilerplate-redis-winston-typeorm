b91b278b698ba4e1b8e8b1648bbd1d78d271528c
dontthinkabouttoomuch <make.abundant@gmail.com>
2023-02-05 16:33:24 +0900

    Init: redis module 설정

---
+++ src/database/redis/interfaces/redis.interface.ts
@@ -0,0 +1,5 @@
+export abstract class IRedisService {
+    get: (key: string) => Promise<string | undefined>;
+    set: (key: string, value: string) => Promise<string | undefined>;
+  }
+  
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { IRedisService } from 'src/database/redis/interfaces/redis.interface';
 import { IUserController } from './interfaces/userController.interface';
 import { IUserService } from './interfaces/userService.interface';
 
 @Controller('user')
 export class UserController implements IUserController {
-  constructor(private readonly userService: IUserService) {}
+  constructor(
+    private readonly userService: IUserService,
+    private readonly redisService: IRedisService
+  ) {}
 
   @Get()
-  findAll() {
+  async findAll() {
+    const result = await this.redisService.set('11', 'bb');
     return this.userService.findAll();
   }
 }
